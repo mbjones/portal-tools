95bfc9dc39d7b9471d6fbc7a05230262e3749252
Matt Jones <gitcode@magisa.org>
2020-08-31 21:28:10 -0800

    Get documents from the REPO rather than the CN.

--- get-portal-doc.sh
+++ get-portal-doc.sh
@@ -2,9 +2,14 @@
 # Usage: get-portal-doc.sh portal-label
 LABEL=$1
 SERVICE="https://cn.dataone.org/cn/v2/"
-PID=`curl -s ${SERVICE}/query/solr/?q=\(label:%22${LABEL}%22%20OR%20seriesId:%22${LABEL}%22\)%20AND%20-obsoletedBy:*\&fl=seriesId,id,label,datasource,obsoletes,obsoletedBy\&sort=dateUploaded%20asc\&rows=100\&wt=json | jq -r '.response.docs[0].id'`
+REPO="https://mn-ucsb-1.dataone.org/knb/d1/mn/v2"
+JSON=`curl -s ${REPO}/query/solr/?q=\(label:%22${LABEL}%22%20OR%20seriesId:%22${LABEL}%22\)%20AND%20-obsoletedBy:*\&fl=seriesId,id,label,datasource,obsoletes,obsoletedBy\&sort=dateUploaded%20asc\&rows=100\&wt=json`
+PID=`echo ${JSON} | jq -r '.response.docs[0].id'`
+NEWLABEL=`echo ${JSON} | jq -r '.response.docs[0].label'`
+#echo "New label: ${NEWLABEL}"
+
 echo "Getting PID: ${PID}"
-curl -s ${SERVICE}/meta/${PID} -o ${LABEL}-sysmeta.xml
-echo "Saved system metadata doc to ${LABEL}-sysmeta.xml"
-curl -s ${SERVICE}/object/${PID} -o ${LABEL}-portal.xml
-echo "Saved portal doc to ${LABEL}-portal.xml."
+curl -s ${REPO}/meta/${PID} -o ${NEWLABEL}-sysmeta.xml
+echo "Saved system metadata doc to ${NEWLABEL}-sysmeta.xml"
+curl -s ${REPO}/object/${PID} -o ${NEWLABEL}-portal.xml
+echo "Saved portal doc to ${NEWLABEL}-portal.xml."
