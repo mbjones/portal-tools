8331cc2c37d417c13fe228118c3f10a0a5ea33d8
Matt Jones <gitcode@magisa.org>
2020-08-31 18:39:03 -0800

    Get both the portal doc and sysmeta from the MN

--- get-portal-doc.sh
+++ get-portal-doc.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 # Usage: get-portal-doc.sh portal-label
 LABEL=$1
-curl https://cn.dataone.org/cn/v2/query/solr/?q=\(label:%223842910%22%20OR%20seriesId:%223842910%22\)%20AND%20-obsoletedBy:*\&fl=seriesId,id,label,datasource,obsoletes,obsoletedBy\&sort=dateUploaded%20asc\&rows=100\&wt=json | jq -r '.response.docs[0].id'
-#urn:uuid:72f9b6c5-8c45-4415-ae1e-4d627904c2db
+SERVICE="https://cn.dataone.org/cn/v2/"
+PID=`curl -s ${SERVICE}/query/solr/?q=\(label:%22${LABEL}%22%20OR%20seriesId:%22${LABEL}%22\)%20AND%20-obsoletedBy:*\&fl=seriesId,id,label,datasource,obsoletes,obsoletedBy\&sort=dateUploaded%20asc\&rows=100\&wt=json | jq -r '.response.docs[0].id'`
+echo "Getting PID: ${PID}"
+curl -s ${SERVICE}/meta/${PID} -o ${LABEL}-sysmeta.xml
+echo "Saved system metadata doc to ${LABEL}-sysmeta.xml"
+curl -s ${SERVICE}/object/${PID} -o ${LABEL}-portal.xml
+echo "Saved portal doc to ${LABEL}-portal.xml."
